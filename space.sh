3e7724a17572960c76694208a4ba052e36e5a232
Leila <leilaticiane@gmail.com>
2023-12-03 18:27:26 +0000

    I'll explain when you're older!

---
+++ space.sh
@@ -0,0 +1,7 @@
+Script started on Sun Dec  3 17:33:03 2023
+^DRestored session: Dom  3 Dez 2023 17:30:44 WET
+[1m[7m%[27m[1m[0m                                                                                ]7;file://MacBook-Air-2.local/Users/leiladantas/Desktop/CommonCore/pipex]2;leiladantas@MacBook-Air-2]1;..monCore/pipex]7;file://MacBook-Air-2.local/Users/leiladantas/Desktop/CommonCore/pipex\[0m[27m[24m[J[01;32m➜ [01;32m [36mpipex [01;34mgit:([31morganize[34m)[33m ✗ [00m[00m[01;34m[00m[K[?1h=[?2004h[?2004l
+
+Saving session...completed.
+
+Script done on Sun Dec  3 17:33:04 2023
