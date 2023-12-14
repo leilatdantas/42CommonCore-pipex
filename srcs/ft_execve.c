84737170c56a06e6ac478f26b420d8ca39924d4e
Leila <leilaticiane@gmail.com>
2023-12-14 19:46:38 +0000

    <html> <head><title>301 Moved Permanently</title></head> <body> <center><h1>301 Moved Permanently</h1></center> </body> </html>

--- srcs/ft_execve.c
+++ srcs/ft_execve.c
@@ -6,7 +6,7 @@
 /*   By: lebarbos <lebarbos@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 20:32:06 by lebarbos          #+#    #+#             */
-/*   Updated: 2023/12/12 16:33:08 by lebarbos         ###   ########.fr       */
+/*   Updated: 2023/12/14 19:45:43 by lebarbos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,34 +66,23 @@ void	ft_execve(char *cmd, char **args, t_pipex *pipex, char **envp)
 
 void	child_process(int *fd, t_pipex *pipex, char **envp)
 {
+	if (pipex->path_cmd2 == NULL)
+	{
+		if (pipex->args_cmd2 != NULL)
+		{
+			if (!ft_strnstr(pipex->args_cmd2[0], ".sh", ft_strlen(pipex->args_cmd2[0])))
+				pipex->path_cmd2 = ft_strdup(pipex->args_cmd2[0]);
+		}
+		ft_cleanup(pipex);
+		ft_putstr_fd("command not found\n", 2);
+		exit(127);	
+	}
 	dup2(pipex->fd_infile, STDIN_FILENO);
 	dup2(fd[1], STDOUT_FILENO);
 	close(fd[0]);
 	close(fd[1]);
 	unlink(URANDOM_PATH);
-	// if (pipex->path_cmd1 == NULL)
-	// {
-	// 	ft_cleanup(pipex);
-	// 	ft_putstr_fd("command not found\n", 2);
-	// 	exit(127);
-	// }
 	ft_execve(pipex->path_cmd1, pipex->args_cmd1, pipex, envp);
-	// else if(execve(pipex->path_cmd1, pipex->args_cmd1, envp) == -1)
-	// {
-	// 	if (access(pipex->path_cmd1, X_OK) == -1)
-	// 	{
-	// 		if (access(pipex->path_cmd1, F_OK) == -1)
-	// 		{
-	// 			if (pipex->args_cmd1[0][0] == '/')
-	// 				custom_error(pipex->args_cmd1[0],
-	// 						"No such file or directory", pipex, 127);
-	// 			custom_error(pipex->args_cmd1[0],
-	// 					"command not found", pipex, 127);
-	// 		}
-	// 		perror(pipex->args_cmd1[0]);
-	// 		exit(126);
-	// 	}
-	// }
 }
 
 void	setup_outfile(t_pipex *pipex, char **argv)
@@ -112,6 +101,17 @@ void	setup_outfile(t_pipex *pipex, char **argv)
 
 void	parent_process(int *fd, t_pipex *pipex, char **envp)
 {
+		if (pipex->path_cmd2 == NULL)
+		{
+			if (pipex->args_cmd2 != NULL)
+			{
+				if (!ft_strnstr(pipex->args_cmd2[0], ".sh", ft_strlen(pipex->args_cmd2[0])))
+					pipex->path_cmd2 = ft_strdup(pipex->args_cmd2[0]);
+			}
+			ft_cleanup(pipex);
+			ft_putstr_fd("command not found\n", 2);
+			exit(127);	
+		}
 		dup2(fd[0], STDIN_FILENO);
 		dup2(pipex->fd_outfile, STDOUT_FILENO);
 		close(fd[1]);
@@ -155,27 +155,20 @@ void	ft_exec(t_pipex *pipex, char **envp, char **argv)
 	if (process == 0)
 	{
 		setup_infile(pipex, argv);
-		if (pipex->fd_infile == -1)
-		{
-			perror(argv[INFILE]);
-			ft_cleanup(pipex);
-			exit(1);
-		}
-		if (pipex->path_cmd1 == NULL && !ft_strnstr(pipex->args_cmd1[0], ".sh", ft_strlen(pipex->args_cmd1[0])))
-			pipex->path_cmd1 = ft_strdup(pipex->args_cmd1[0]);
+		// if (pipex->fd_infile == -1)
+		// {
+		// 	perror(argv[INFILE]);
+		// 	ft_cleanup(pipex);
+		// 	exit(1);
+		// }
+		// if (pipex->path_cmd1 == NULL && !ft_strnstr(pipex->args_cmd1[0], ".sh", ft_strlen(pipex->args_cmd1[0])))
+		// 	pipex->path_cmd1 = ft_strdup(pipex->args_cmd1[0]);
 		child_process(fd, pipex, envp);
 	}
 	else
 	{
 		waitpid(process, NULL, WNOHANG);
 		setup_outfile(pipex, argv);
-		if (pipex->path_cmd1 == NULL && !ft_strnstr(pipex->args_cmd1[0], ".sh", ft_strlen(pipex->args_cmd1[0])))
-		{
-			pipex->path_cmd2 = ft_strdup(pipex->args_cmd2[0]);
-			// ft_cleanup(pipex);
-			// ft_putstr_fd("command not found\n", 2);
-			// exit(127);
-		}
 		parent_process(fd, pipex, envp);
 	}
 }
